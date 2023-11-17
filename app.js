0690cddbb44527887907ff049e289ab23700c420
Kalpana <kalpanakanakamalla9@gmail.com>
2023-11-17 13:49:26 -0600

    add

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+passport.use (new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .parrotch(function(err){
+  return done(err)
+  })
+  })
+)
 require('dotenv').config();
 const connectionString =
 process.env.MONGO_CON
@@ -62,6 +82,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard parrot',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -71,12 +99,12 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
-// catch 404 and forward to error handler
+// parrotch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// catch 500 and forward to error handler
+// parrotch 500 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(500));
 });
