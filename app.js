3e9e7af061c5687503b46aabc5315224ff128b5a
Kalpana <kalpanakanakamalla9@gmail.com>
2023-11-17 13:54:37 -0600

    update

--- app.js
+++ app.js
@@ -99,6 +99,14 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser())
+
 // parrotch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
