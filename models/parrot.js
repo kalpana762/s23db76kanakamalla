7089ff566a26eaa014b5ffe0a26b47f91bfdc9cc
Kalpana <kalpanakanakamalla9@gmail.com>
2023-11-29 12:29:52 -0600

    commit

--- models/parrot.js
+++ models/parrot.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
-const costumeSchema = mongoose.Schema({
+const parrotSchema = mongoose.Schema({
 parrot_color: String,
-parrot_breed: String,
-parrot_price: Number
+parrot_breed: {type:String,length:15},
+parrot_price: {type:Number,min:1000,max:100000}
 })
-module.exports = mongoose.model("parrot",costumeSchema)
+module.exports = mongoose.model("parrot",parrotSchema)
--- routes/parrot.js
+++ routes/parrot.js
@@ -7,11 +7,20 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const parrot_controlers= require('../controllers/parrot');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  res.redirect("/login");
+  }
 /* GET parrots */
 router.get('/', parrot_controlers.parrot_view_all_Page );
 router.get('/detail', parrot_controlers.parrot_view_one_Page);
 router.get('/create', parrot_controlers.parrot_create_Page);
-router.get('/update', parrot_controlers.parrot_update_Page);
+router.get('/update',secured, parrot_controlers.parrot_update_Page);
 router.get('/delete', parrot_controlers.parrot_delete_Page);
 
 
